eb5e73059fe0a570778b6fab144f11e5e8f1a31b
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2022-08-30 16:06:02 +0530

    Save response body and use it in subsequent requests

--- doc/5-script-fundamentals.md
+++ doc/5-script-fundamentals.md
@@ -102,3 +102,36 @@ class CheckResponseBodyAndExtract extends Simulation{
   ).protocols(httpProtocol)
 }
 ```
+
+## Saving Response body and using it
+
+```scala
+package videogamedb.scriptsfundamentals
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class CheckResponseBodyAndExtract extends Simulation{
+  val httpProtocol = http.baseUrl("https://www.videogamedb.uk/api")
+    .acceptHeader("application/json")
+
+  val scn = scenario("Check with json path and extract")
+
+    .exec(http("Get Specific game")
+      .get("/videogame/1")
+      .check(status.in(200 to 210), jsonPath("$.name").is("Resident Evil 4")))
+
+    .exec(http("Get All Video Games")
+    .get("/videogame")
+    .check(jsonPath("$[1].id").saveAs("gameId")))
+
+    .exec(http("Get specific game")
+      .get("/videogame/#{gameId}") // using the saved paramter
+    .check(jsonPath("$.name").is("Gran Turismo 3")))
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpProtocol)
+}
+
+```
--- src/test/scala/videogamedb/scriptsfundamentals/CheckResponseBodyAndExtract.scala
+++ src/test/scala/videogamedb/scriptsfundamentals/CheckResponseBodyAndExtract.scala
@@ -3,30 +3,25 @@ package videogamedb.scriptsfundamentals
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-import scala.concurrent.duration.DurationInt
-
-class CheckResponseCode extends Simulation {
-
+class CheckResponseBodyAndExtract extends Simulation{
   val httpProtocol = http.baseUrl("https://www.videogamedb.uk/api")
     .acceptHeader("application/json")
 
-  val scn = scenario("Video Game DB - 3 Calls")
-    .exec(http("Get all video games - 1st call")
-      .get("/videogame")
-      .check(status.is(200)))
-    .pause(5)
+  val scn = scenario("Check with json path and extract")
 
     .exec(http("Get Specific game")
       .get("/videogame/1")
-      .check(status.in(200 to 210)))
-    .pause(1, 10)
+      .check(status.in(200 to 210), jsonPath("$.name").is("Resident Evil 4")))
 
-    .exec(http("Get all video games - 2nd call")
-      .get("/videogame")
-      .check(status.not(404), status.not(500)))
-    .pause(3000.milliseconds)
+    .exec(http("Get All Video Games")
+    .get("/videogame")
+    .check(jsonPath("$[1].id").saveAs("gameId")))
 
-    setUp(
-      scn.inject(atOnceUsers(1))
-    ).protocols(httpProtocol)
+    .exec(http("Get specific game")
+      .get("/videogame/#{gameId}") // using the saved paramter
+    .check(jsonPath("$.name").is("Gran Turismo 3")))
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpProtocol)
 }
