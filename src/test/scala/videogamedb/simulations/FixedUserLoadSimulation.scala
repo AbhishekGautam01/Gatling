c9b0a7137314ee9b55900d573d1a7ad0e7cde8ac
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2022-09-01 05:57:48 +0530

    RampUserPerSec & ConstantUserPerSecond

---
+++ src/test/scala/videogamedb/simulations/FixedUserLoadSimulation.scala
@@ -0,0 +1,38 @@
+package videogamedb.simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+class FixedUserLoadSimulation extends Simulation {
+  var httpProtocol = http.baseUrl("https://www.videogamedb.uk:443/api")
+    .acceptHeader("application/json")
+
+  def getAllVideoGames(): ChainBuilder = {
+    exec(
+      http("GetAllVideo Games")
+        .get("/videogame")
+    )
+  }
+  def getSpecificVideoGames(): ChainBuilder = {
+    exec(
+      http("GetAllVideo Games")
+        .get("/videogame/2")
+    )
+  }
+
+  val scn = scenario("Basic Load Simulation")
+    .exec(getAllVideoGames())
+    .pause(2)
+    .exec(getSpecificVideoGames())
+    .pause(5)
+    .exec(getAllVideoGames())
+
+  setUp(
+    scn.inject(
+      nothingFor(5),
+      constantUsersPerSec(10).during(10), //Here the time is in seconds
+      rampUsersPerSec(1).to(5).during(5)
+    ).protocols(httpProtocol)
+  )
+}
